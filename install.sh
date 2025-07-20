417b05be3d9506017cda36286592eb68a8d2ac77
Mafrobo <mattis.bonfico@t-online.de>
2025-07-20 15:49:51 +0200

    init menu

--- install.sh
+++ install.sh
@@ -1,8 +1,29 @@
 #!/bin/bash 
 
 root_menu() { 
-	myvar=$(gum choose "init" "mirrors" "locals" "disk" "kernel" "hostname" "root passwd")
+	select_menu=$(gum choose "init" "mirrors" "locals" "disk" "kernel" "hostname" "root passwd")
+	case $select_menu in 
+		"init") init_menu;;
+		"mirrors") mirrors_menu;;
+		"locals") locals_menu;;
+		"disk") disk_menu;;
+		"kernel") kernel_menu;;
+		"hostname") hostname_menu;;
+		"root passwd") root_passwd_menu;;
+	esac
 }
-root_menu 
+init_menu() { 
+	select_menu=$(gum choose "return" "dinit" "openrc" "s6-rc" "runit" "Systemd")
+	case $select_menu in 	
+		"return <--") root_menu;;
+		"dinit") init_system=1;  root_menu;;
+		"openrc") init_system=2;  root_menu;;
+		"s6-rc") init_system=3;  root_menu;;
+		"runit") init_system=4;  root_menu;;
+		"Systemd") init_system=5;  root_menu;;
+	esac  
+} 
+mirrors_menu() { 
+	selcet_menu=$(gum choose "return"   )
+}      	
 root_menu
-echo $myvar
