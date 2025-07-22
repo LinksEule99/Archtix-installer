7ec23154d2a88c919d0a8aba4a845cb8a1c54eb4
Mafrobo <mattis.bonfico@t-online.de>
2025-07-22 19:04:49 +0200

    local menu

--- install.sh
+++ install.sh
@@ -25,10 +25,47 @@ init_menu() {
 } 
 mirrors_menu() { 
 	countries=$(reflector --list-countries | sed 's/^[[:space:]]*[0-9]*[[:space:]]*//')
-	#selcet_menu=$(gum choose "return" "$countries" ) 
 	selected_country=$(printf "return\n%s" "$countries" | gum choose)
 	if [[ "$selected_country" == "return" ]]; then
         	root_menu  
+		$selceted_country = none
+	else 
+		root_menu
    	fi
-}      	
+} 
+locals_menu() {
+	keyboard_menu() {
+	      	mapfile -t keymaps < <(find /usr/share/kbd/keymaps/ -type f -name "*.map.gz" \
+  | sed 's|.*/||;s|\.map\.gz$||' | sort)
+		keyboard=$( printf "return\n%s" "${keymaps[@]}" | gum choose)
+		if [ "$keyboard" != "return" ];then
+			loadkeys "$keyboard"
+		fi 
+		locals_menu
+	}
+	timezone_menu() { 
+		continent() { 	
+			mapfile -t city< <(find /usr/share/zoneinfo/"$1" -type f | sed 's|/usr/share/zoneinfo/||' | sort)
+			timezone=$(printf "return\n%s" "${city}" | gum choose)
+
+
+
+		
+		mapfile -t continent< <(find /usr/share/zoneinfo/ -maxdepth 1 -type d | tail -n +2 | sed 's|.*/||')
+		selcet_menu=$( printf "return\n%s" "${continent[@]}" | gum choose)
+		continent $selcet_menu 
+		unset -f continent
+		locals_menu
+		
+	}
+	select_menu=$(gum choose "return" "keyboard" "timezone" "language")
+	case $select_menu in 
+		"return") root_menu; unset -f keyboard_menu; unset -f timezone_menu; unset -f language_menu;;
+		"keyboard") keyboard_menu;;
+		"timezone") timezone_menu;;
+		"language") language_menu;;
+	esac
+
+
+}
 root_menu
